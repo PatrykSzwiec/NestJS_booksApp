10a4d87f03d3516495a0af6a06e49e9ef16e15cc
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-10-16 22:08:02 +0200

    Add configuration for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorsModule } from './authors/authors.module';
 import { BooksModule } from './books/books.module';
-import { UsersController } from './users/users/users.controller';
-import { UsersService } from './users/users/users.service';
-import { UsersModule } from './users/users/users.module';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthorsModule, BooksModule, UsersModule],
+  imports: [AuthorsModule, BooksModule, UsersModule, AuthModule, PrismaModule],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
